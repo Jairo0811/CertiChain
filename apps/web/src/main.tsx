5a03721cb1a4ee3b603bcd71f735b024bdfc9631
Jairo Matías <jairo_1829@hotmail.com>
2026-09-06 18:11:02 -0400

    feat(web): load CertiChain branding styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import { PublicVerify } from "./PublicVerify";
 import "./styles.css";
 import "./fontawesome-icons.css";
+import "./branding.css";
 
 const isPublicVerification = window.location.pathname === "/verify";
 
